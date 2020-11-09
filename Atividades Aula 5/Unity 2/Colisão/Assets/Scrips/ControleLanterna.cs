781da8d020003fced278eec036a22d10d57c7ef8
Rick-Dassi <h172194@dac.unicamp.br>
2020-11-08 22:51:25 -0300

    Unity 2

---
+++ Atividades Aula 5/Unity 2/Colisão/Assets/Scrips/ControleLanterna.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleLanterna : MonoBehaviour
+{
+    public bool isOn = false;
+    public GameObject lightSource;
+    public bool failSafe = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("TeclaF"))
+        {
+            if (isOn == false && failSafe == false)
+            {
+                failSafe = true;
+                lightSource.SetActive(true);
+                isOn = true;
+                StartCoroutine(FailSafe());
+            }
+            if (isOn == true && failSafe == false)
+            {
+                failSafe = true;
+                lightSource.SetActive(false);
+                isOn = false;
+                StartCoroutine(FailSafe());
+            }
+        }
+    }
+
+    IEnumerator FailSafe()
+    {
+        yield return new WaitForSeconds(0.25f);
+        failSafe = false;
+    }
+}
---
+++ Atividades Aula 5/Unity 2/Colisão/Assets/Scrips/PortaSimples.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortaSimples : MonoBehaviour
+{
+    public Animator _animator;
+
+    private bool _colidindo;
+    private bool _portaAbera = false;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.E) && _colidindo)
+        {
+            _portaAbera = true;
+            _animator.SetTrigger ("Abrir");
+        }
+    }
+
+    void OnTriggerEnter (Collider _col)
+    {
+        if (_col.gameObject.CompareTag ("Door"))
+        {
+            _colidindo = true;
+        }
+    }
+
+    void OnTriggerExit (Collider _col)
+    {
+        if (_col.gameObject.CompareTag ("Door"))
+        {
+            if (_portaAbera)
+            {
+                _animator.SetTrigger ("Fechar");
+            }
+
+            _colidindo = false;
+        }
+    }
+}
